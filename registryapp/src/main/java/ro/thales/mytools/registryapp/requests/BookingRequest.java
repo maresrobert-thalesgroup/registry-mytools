4218510e9f0d167f19f997c2be00ab914bfd3e6f
poparobert98 <robert.popa@thalesgroup.com>
2021-09-20 12:21:22 +0300

    Add validation

--- registryapp/src/main/java/ro/thales/mytools/registryapp/requests/BookingRequest.java
+++ registryapp/src/main/java/ro/thales/mytools/registryapp/requests/BookingRequest.java
@@ -3,16 +3,38 @@ package ro.thales.mytools.registryapp.requests;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.*;
 import java.util.Date;
 
 @AllArgsConstructor
 @Data
 public class BookingRequest {
+
+    @NotNull(message = "request_by_id cannot be null")
+    @Min(value=1, message="request_by_id must be equal or greater than 1")
     private Long request_by_id;
+
+    @NotNull(message = "request_for_id cannot be null")
+    @Min(value=1, message="request_for_id must be equal or greater than 1")
     private Long request_for_id;
+
+    @NotNull(message = "startDate cannot be null")
     private Date startDate;
+
+    @NotNull(message = "endDate cannot be null")
     private Date endDate;
+
+    @NotNull(message = "accessFloors cannot be null")
+    @NotEmpty(message = "accessFloors cannot be empty")
     private int[] accessFloors;
+
+    @NotNull(message = "kitNeeded cannot be null")
+    @NotEmpty(message = "kitNeeded cannot be empty")
+    @Pattern(regexp = "\\A(?!\\s*\\Z).+", message = "kitNeeded cannot be whitespaces")
     private String kitNeeded;
+
+    @NotNull(message = "status cannot be null")
+    @Min(value=0, message="status be equal or greater than 0")
+    @Max(value=2, message="status be equal or less than 2")
     private Integer status;
 }
--- registryapp/src/main/java/ro/thales/mytools/registryapp/requests/TemplateRequest.java
+++ registryapp/src/main/java/ro/thales/mytools/registryapp/requests/TemplateRequest.java
@@ -4,6 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.io.Serializable;
 
 @AllArgsConstructor
@@ -11,8 +15,20 @@ import java.io.Serializable;
 @Data
 public class TemplateRequest implements Serializable {
 
+    @NotNull(message = "requestById cannot be null")
+    @Min(value=1, message="requestById must be equal or greater than 1")
     private Long requestById;
+
+    @NotNull(message = "requestForId cannot be null")
+    @Min(value=1, message="requestForId must be equal or greater than 1")
     private Long requestForId;
+
+    @NotNull(message = "floorAccess cannot be null")
+    @NotEmpty(message = "floorAccess cannot be empty")
     private int[] floorAccess;
+
+    @NotNull(message = "kitRequired cannot be null")
+    @NotEmpty(message = "kitRequired cannot be empty")
+    @Pattern(regexp = "\\A(?!\\s*\\Z).+", message = "kitRequired cannot be whitespaces")
     private String kitRequired;
 }
